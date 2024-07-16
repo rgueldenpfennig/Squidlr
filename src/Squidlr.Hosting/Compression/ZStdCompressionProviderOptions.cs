e38b2f8511fb7ae9db7a715e66c9a1a61865c05d
rgueldenpfennig <mail@robin-gueldenpfennig.de>
2024-07-16 17:19:16 +0200

    feat(web): add ZStandard (Zstd) compression for web frontend

---
+++ src/Squidlr.Hosting/Compression/ZStdCompressionProviderOptions.cs
@@ -0,0 +1,18 @@
+﻿using System.IO.Compression;
+using Microsoft.Extensions.Options;
+
+namespace Squidlr.Hosting.Compression;
+
+/// <summary>
+/// Options for the <see cref="ZstdCompressionProvider"/>
+/// </summary>
+public sealed class ZstdCompressionProviderOptions : IOptions<ZstdCompressionProviderOptions>
+{
+    /// <summary>
+    /// What level of compression to use for the stream. The default is <see cref="CompressionLevel.Fastest"/>.
+    /// </summary>
+    public CompressionLevel Level { get; set; } = CompressionLevel.Fastest;
+
+    /// <inheritdoc />
+    ZstdCompressionProviderOptions IOptions<ZstdCompressionProviderOptions>.Value => this;
+}
