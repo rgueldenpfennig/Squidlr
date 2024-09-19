dd011c6cf46835e4129e6c2dc028fe4d929bb340
rgueldenpfennig <mail@robin-gueldenpfennig.de>
2024-09-19 08:24:55 +0200

    add LinkedIn UrlUtilitiesTests

---
+++ test/Squidlr.Tests/LinkedIn/Utilities/UrlUtilitiesTests.cs
@@ -0,0 +1,36 @@
+using Squidlr.LinkedIn.Utilities;
+
+namespace Squidlr.Tests.LinkedIn.Utilities;
+
+public class UrlUtilitiesTests
+{
+    [Theory]
+    [InlineData("https://www.linkedin.com/posts/william-is-saying-ugcPost-7210720852944867329-nmqI/?foo=bar#abc", true)]
+    [InlineData("https://www.linkedin.com/posts/foo-7210720852944867329-foo", true)]
+    [InlineData("https://linkedin.com/posts/foo-54534225453422-abc", true)]
+    [InlineData("https://linkedin.com/video/foo-5453422-abc", false)]
+    [InlineData("https://linkedin.com/video/foo-abc", false)]
+    [InlineData("https://example.com/foo/1152128691131318273/", false)]
+    [InlineData("https://google.com", false)]
+    [InlineData("invalid-url", false)]
+    public void IsValidLinkedInUrl(string url, bool expectedResult)
+    {
+        // Act
+        var result = UrlUtilities.TryGetLinkedInIdentifier(url, out _);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+
+    [Theory]
+    [InlineData("https://www.linkedin.com/posts/william-is-saying-ugcPost-7210720852944867329-nmqI/?foo=bar#abc", "7210720852944867329", "https://www.linkedin.com/posts/william-is-saying-ugcPost-7210720852944867329-nmqI")]
+    public void GetLinkedInIdentifier(string url, string expectedId, string expectedUrl)
+    {
+        // Act
+        Assert.True(UrlUtilities.TryGetLinkedInIdentifier(url, out var identifier));
+
+        // Assert
+        Assert.Equal(expectedId, identifier.Value.Id);
+        Assert.Equal(expectedUrl, identifier.Value.Url);
+    }
+}
