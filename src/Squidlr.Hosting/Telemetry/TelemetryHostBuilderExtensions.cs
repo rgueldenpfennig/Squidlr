46551570d40f6922f2bdb61bd4fb9a9c798abeff
rgueldenpfennig <mail@robin-gueldenpfennig.de>
2024-11-18 15:54:18 +0100

    telemetry: disable AI adaptive sampling

--- src/Squidlr.Hosting/Telemetry/TelemetryHostBuilderExtensions.cs
+++ src/Squidlr.Hosting/Telemetry/TelemetryHostBuilderExtensions.cs
@@ -1,22 +1,22 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 
-namespace Squidlr.Hosting.Telemetry
-{
-    public static class TelemetryHostBuilderExtensions
-    {
-        public static IServiceCollection AddTelemetry(this IServiceCollection services, Action<TelemetryOptions> options)
-        {
-            services.AddApplicationInsightsTelemetry(options =>
-            {
-                options.EnableActiveTelemetryConfigurationSetup = true;
-                options.EnableQuickPulseMetricStream = true;
-            });
-            services.AddServiceProfiler();
-            services.AddApplicationInsightsTelemetryProcessor<IgnoreProfilerDependencyTelemetryProcessor>();
-            services.AddApplicationInsightsTelemetryProcessor<IgnorePathTelemetryProcessor>();
-            services.Configure(options);
+namespace Squidlr.Hosting.Telemetry;
 
-            return services;
-        }
+public static class TelemetryHostBuilderExtensions
+{
+    public static IServiceCollection AddTelemetry(this IServiceCollection services, Action<TelemetryOptions> options)
+    {
+        services.AddApplicationInsightsTelemetry(options =>
+        {
+            options.EnableActiveTelemetryConfigurationSetup = true;
+            options.EnableQuickPulseMetricStream = true;
+            options.EnableAdaptiveSampling = false;
+        });
+        services.AddServiceProfiler();
+        services.AddApplicationInsightsTelemetryProcessor<IgnoreProfilerDependencyTelemetryProcessor>();
+        services.AddApplicationInsightsTelemetryProcessor<IgnorePathTelemetryProcessor>();
+        services.Configure(options);
+
+        return services;
     }
 }
